2d905bd181f75a137254ec89cb7b34622f71ca63
tunnckoCore <mameto_100@mail.bg>
2015-07-17 15:01:16 +0300

    add more tests, ref #4

--- test.js
+++ test.js
@@ -20,7 +20,7 @@ it('should throw if not array, string, regexp, object or function', function () 
 })
 
 describe('should return a matching function:', function () {
-  it('from an object:', function () {
+  it('from an object', function () {
     var isMatch = matcher({a: 'b'});
 
     isMatch({a: 'b'}).should.be.true;
@@ -28,7 +28,7 @@ describe('should return a matching function:', function () {
     isMatch({e: 'f', c: 'd'}).should.be.false;
   });
 
-  it('from a string:', function () {
+  it('from a string', function () {
     var isMatch = matcher('a');
 
     isMatch('a').should.be.true;
@@ -37,36 +37,48 @@ describe('should return a matching function:', function () {
     matcher('a')('def').should.be.false;
   });
 
-  it('from a string glob pattern:', function () {
+  it('from a string glob pattern on single value', function () {
     matcher('*')('a').should.be.true;
     matcher('!b')('a').should.be.true;
     matcher('!b')('b').should.be.false;
   });
 
-  it('from a string non-glob pattern:', function () {
+  it('from a string glob pattern on multiple values', function () {
+    matcher('*')(['a', 'b']).should.be.true;
+    matcher('{(b|[A-Z]),!([C-F])}.js')(['E.js', 'a.js']).should.be.false;
+    matcher('{(b|[A-Z]),!([C-F])}.js')(['E.js', 'B.js']).should.be.true;
+  });
+
+  it('from a string non-glob pattern on single value', function () {
     matcher('abc')('foo abc bar').should.be.false;
     matcher('abc')('abc').should.be.true;
+  })
+
+  it('from a string non-glob pattern on multiple values', function () {
     matcher('abc')(['foo', 'abc']).should.be.true;
     matcher('abc')(['foo abc bar', 'baz']).should.be.true;
   });
 
-  it('from an array of glob patterns:', function () {
-    matcher(['a'])('a').should.be.false;
-    matcher(['b'])('a').should.be.false;
-    matcher(['b', 'a'])(['a', 'b']).should.be.false;
-    matcher(['b', 'a'])(['b', 'a']).should.be.true;
-    matcher(['b', 'c', '*'])(['b', 'c', '*']).should.be.true;
+  it('from an array of glob patterns on single value', function () {
+    matcher(['a', 'b'])('a').should.be.true;
+    matcher(['a', 'b'])('c').should.be.false;
     matcher(['*-koaip', '!foo-koa'])('x-koaip').should.be.true;
+  });
+
+  it('from an array of glob patterns on multiple values', function () {
+    matcher(['a', 'b'])(['a', 'c']).should.be.true;
+    matcher(['a', 'b'])(['e', 'f', 'g']).should.be.false;
+    matcher(['*-koaip', '!foo-koa'])(['x-koaip', 'bar-koa']).should.be.true;
     matcher(['*-koaip', '!foo-koa'])(['foo-koa', 'bar-koa']).should.be.false;
   });
 
-  it('from a regex:', function () {
+  it('from a regex', function () {
     var isMatch = matcher(/a/);
     isMatch('a').should.be.true;
     isMatch('b').should.be.false;
   });
 
-  it('from a function:', function () {
+  it('from a function', function () {
     var isMatch = matcher(function  (val) {
       return val === 'a';
     });
